19031604912678c149744651ebb3e3bfa9864f0c
ArshiMahajan22 <77196068+ArshiMahajan22@users.noreply.github.com>
2021-11-07 12:37:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function preload () {
 
 function setup(){
   var canvas =  createCanvas(700,600);
-  canvas.parent('CanvasHolder');
+  canvas.parent('#CanvasHolder');
   video = createCapture(VIDEO);
   video.size(700, 600)
   video.hide();
